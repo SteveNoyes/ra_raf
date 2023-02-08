8ece495feff0565d0b9bd3d7e57cb3e78f159ccb
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-02-07 21:21:05 -0500

    cover page

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var password = document.getElementById("inputPassword");
 var username = document.getElementById("inputUsername");
 
-
 document.getElementById("login_redirect").onclick = function () {
   if( !password.value || !username.value) {
     location.href = "./pages/prizes.html";
   }
-  
 };
