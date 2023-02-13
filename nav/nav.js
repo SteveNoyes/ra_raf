07e97c42ad05605bc93e6e76aee108025ab72fe4
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-02-12 21:15:38 -0500

    cover nav

---
+++ nav/nav.js
@@ -0,0 +1,10 @@
+var overlay = document.getElementById("overlay");
+
+var closeMenu = document.getElementById("close-menu");
+
+document.getElementById('open-menu').addEventListener("click", function() {
+  overlay.classList.add('show-menu');
+});
+document.getElementById('close-menu').addEventListener("click", function() {
+  overlay.classList.remove('show-menu');
+});
