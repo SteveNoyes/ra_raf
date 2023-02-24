a0fa0f0cba81c1def52333a586f4e238befeca76
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-02-23 20:47:27 -0500

    ready for php integration

--- app.js
+++ app.js
@@ -6,33 +6,3 @@ document.getElementById("login_redirect").onclick = function () {
     location.href = "./pages/prizes.html";
   }
 };
-
-
-
-
-
-
-function animateValue(item, duration, start = 0) {
-  var end = item.innerText.replaceAll(",", "").trim();
-  var range = end - start;
-  var increment = (range / (duration / 10));
-  
-  if(end < start){
-    increment *= -1;
-  }
-  var current = start;
-  var stepTime = 1;
-  
-  var timer = setInterval(function() {
-    current += Math.ceil(increment);
-    item.innerText = current;
-    if (current >= end) {
-      item.innerText = end;
-      clearInterval(timer);
-    }
-  }, stepTime);
-}
-
-document.querySelectorAll('.value').forEach((item)=>{
-  animateValue(item, 3000);
-});
