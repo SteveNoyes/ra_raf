76c67effa7e2c2e5b6e7c8181700d2ff96dffdb8
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-02-23 20:49:56 -0500

    removed old js file

--- app.js
+++
@@ -1,8 +0,0 @@
-var password = document.getElementById("inputPassword");
-var username = document.getElementById("inputUsername");
-
-document.getElementById("login_redirect").onclick = function () {
-  if( !password.value || !username.value) {
-    location.href = "./pages/prizes.html";
-  }
-};
